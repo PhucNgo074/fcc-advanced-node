af32594457a6dd6e47dc78dd0e89fa07613f483e
Phúc Ngô <phuc12345524@gmail.com>
2025-10-15 17:39:25 +0700

    fix

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.route('/').get((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
