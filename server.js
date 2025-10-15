1d88ddfd6088d3092337c094296033a66612113b
Phúc Ngô <phuc12345524@gmail.com>
2025-10-15 17:46:19 +0700

    test1

--- server.js
+++ server.js
@@ -6,15 +6,16 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', './views/pug');
+
 fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' });
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
